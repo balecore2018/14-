213b32ffe689650b53d16aa68af59d1ff1d0d2e2
dima <balecored@gmail.com>
2026-02-22 15:35:37 +0500

    Создание классов. Шаповалов

---
+++ 14/Classes/Common/CheckRegex.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace _14.Classes.Common
+{
+    public class CheckRegex
+    {
+        public static bool Match(string Input, string Pattern)
+        {
+            Match m = Regex.Match(Input, Pattern);
+            return m.Success;
+        }
+    }
+}
