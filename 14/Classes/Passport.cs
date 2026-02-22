7cf447df6f3a8a3840d4ba4a69199cb21f689261
dima <balecored@gmail.com>
2026-02-22 15:32:43 +0500

    Создание окна добавления/изменения. Шаповалов

---
+++ 14/Classes/Passport.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14.Classes
+{
+    internal class Passport
+    {
+        public string Name {  get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Issued { get; set; }
+        public string DateOfIssued { get; set; }
+        public string DepatmentCode { get; set; }
+        public string SerialAndNumber { get; set; }
+        public string DateOfBirth { get; set; }
+        public string PlaceOfBirth { get; set; }
+    }
+}
